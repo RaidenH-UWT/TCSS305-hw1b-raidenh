e2af99c3f0435f71c24fb422fe61727426fc923a
EndDragon438 <quantumhelix@logandigital.com>
2024-10-09 23:57:52 -0700

    created test classes

---
+++ test/edu/uw/tcss/model/ItemOrderTest.java
@@ -0,0 +1,15 @@
+package edu.uw.tcss.model;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Test;
+
+class ItemOrderTest {
+
+}
